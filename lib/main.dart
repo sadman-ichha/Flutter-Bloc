ee50535368b5f8fa2c3db9257d605e16544e48bd
sadman_ichha <sadmanichha1@gmail.com>
2024-08-04 22:58:45 +0600

    added bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:f_bloc_practice/bloc/text_cubit/cubit/text_change_cubit.dart';
-import 'package:f_bloc_practice/ui/page/home_screen.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'bloc/color_cubit/change_color_cubit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,12 +14,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: "Learn Flutter Bloc ",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (context) => ChangeColorCubit()),
-            BlocProvider(create: (context) => TextChangeCubit()),
-          ],
-          child:const HomeScreen(),
-        ));
+        // home: ,
+        
+        );
   }
 }
