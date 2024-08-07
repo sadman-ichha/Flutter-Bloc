73b93ec7d61ff139be3414252efc50b7934ce26b
sadman_ichha <sadmanichha1@gmail.com>
2024-08-07 10:26:01 +0600

    added bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:f_bloc_practice/cubit/todo_cubit.dart';
-import 'package:f_bloc_practice/my_screen.dart';
 import 'package:f_bloc_practice/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,32 +7,18 @@ void main() {
   runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         title: "Learn Flutter Bloc",
-//         theme: ThemeData(primarySwatch: Colors.blue),
-//         home: BlocProvider(
-//           create: (context) => TodoCubit(),
-//           child: const HomeScreen(),
-//         ));
-//   }
-// }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Learn Flutter Bloc",
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home:const MyScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Learn Flutter Bloc",
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: BlocProvider(
+          create: (context) => TodoCubit(),
+          child: const HomeScreen(),
+        ));
   }
 }
